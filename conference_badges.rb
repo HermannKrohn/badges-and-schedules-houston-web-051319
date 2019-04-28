bffaabae13863ceae4759ba02a4c021cf270b897
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-04-28 17:48:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,9 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
-    array.each_with_index { |person, index|
-  array[person] = "Hello, " + person + "! You'll be assigned to room " + index}
+  returnArray = []
+  for person in array do 
+    returnArray << "Hello, " + person + "! You'll be assigned to room " + person.elementIndex
+  end
+  returnArray
 end
