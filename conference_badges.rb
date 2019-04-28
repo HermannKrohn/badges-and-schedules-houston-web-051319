3190b462bfab93eea82069068c8acc318c8d14ac
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-04-28 18:00:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,3 +18,17 @@ def assign_rooms(array)
   returnArray << "Hello, #{item}! You'll be assigned to room #{index+1}!"}
   returnArray
 end
+
+def printer(array)
+  batchNameArray = batch_badge_creator(array)
+  roomsArray = assign_rooms(array)
+  for nameTag in batchNameArray do 
+    puts nameTag
+  end
+  
+  for room in roomsArray do 
+    puts room
+  end
+end
+  
+  
