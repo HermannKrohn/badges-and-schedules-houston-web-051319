31f22ee4953b8e03739b73e34f6c3529da75a679
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-04-28 17:51:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,12 +14,13 @@ end
 
 def assign_rooms(array)
   returnArray = []
-  for person in array do 
-    returnArray << "Hello, " + person + "! You'll be assigned to room " + person.elementIndex + "!"
-  end
-  returnArray
+  #for person in array do 
+  #  returnArray << "Hello, " + person + "! You'll be assigned to room " + #person.elementIndex + "!"
+ # end
+ # returnArray
   
   array.each_with_index { |item, index|
   returnArray << "Hello, " + item + "! You'll be assigned to room " + index + "!"
 }
+returnArray
 end
